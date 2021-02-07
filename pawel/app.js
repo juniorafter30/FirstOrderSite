822ba7355d83fc982f259c7c4ab556faffbaa64a
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-02-07 12:45:08 +0100

    Some changes

--- pawel/app.js
+++ pawel/app.js
@@ -1,14 +1,19 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav');
-const navLinks = nav.querySelectorAll('a');
-const header = document.querySelector('header')
-const sessionButton = document.querySelector('.session')
+// OUR VARIABLES
+
+const burger = document.querySelector('.burger');  //Navigation burger
+const nav = document.querySelector('nav'); // Navigation
+const navLinks = nav.querySelectorAll('a'); // Navigation links
+const header = document.querySelector('header') // Our whole header
+const sessionButton = document.querySelector('.session') // Session button
+
+//NAVIGATION BURGER FUNCTION
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('active');
     burger.classList.toggle('toggle');
 })
 
+// Function that closes navigation when user clicks on single position
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         nav.classList.toggle('active');
@@ -16,7 +21,9 @@ navLinks.forEach(link => {
     })
 })
 
-window.addEventListener('scroll', ()=>{
+// Changing color of Header while scrolling
+
+window.addEventListener('scroll', () => {
 header.classList.toggle('on-scroll', window.scrollY > 0);
 sessionButton.classList.toggle('on-scroll', window.scrollY > 0);
 
