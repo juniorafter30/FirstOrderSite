512f1d42a4e360cd2e0258e682b14cd1b0c60eed
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-02-09 14:13:41 +0100

    almost done

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+
+// OUR VARIABLES
+
+const burger = document.querySelector('.burger');  //Navigation burger
+const nav = document.querySelector('nav'); // Navigation
+const navLinks = nav.querySelectorAll('a'); // Navigation links
+const header = document.querySelector('header') // Our whole header
+const sessionButton = document.querySelector('.session') // Session button
+const landingMotto= document.querySelector('.landing-text');
+
+
+const tl = new TimelineMax();
+
+//NAVIGATION BURGER FUNCTION
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    burger.classList.toggle('toggle');
+})
+
+// Function that closes navigation when user clicks on single position
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        burger.classList.toggle('toggle');
+    })
+})
+
+// Changing color of Header while scrolling
+
+window.addEventListener('scroll', () => {
+header.classList.toggle('on-scroll', window.scrollY > 0);
+sessionButton.classList.toggle('on-scroll', window.scrollY > 0);
+
+});
+
+
